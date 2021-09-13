218710be8efdf07a567e2079a7f32bee6a6acd4c
TonySiberia <escapefromhive@gmail.com>
2021-09-13 15:05:27 +0700

    Creat MovieDB application

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-/*jshint strict:false */
-"use strict";
 
-let selfQuestion = prompt("what do you do?", "fuck");
-console.log(selfQuestion + " , ohhhh myyyy!!!!");
+const numberOfilms = +prompt("сколько фильмов вы уже посмотрели", "");
+console.log(numberOfilms);
+
+const personalMovieDB = {
+    count: numberOfilms,
+    movies: {},
+    actors: {},
+    Genres: [],
+    private: false
+};
+
+const lastFilm= prompt("один из последних просмотренных фильмов?", ""),
+      lastFilmRait= prompt("на сколько вы его оцените?", ""),
+      lastFilm1= prompt("один из последних просмотренных фильмов?", ""),
+      lastFilmRait1= prompt("на сколько вы его оцените?", "");
+
+personalMovieDB.movies[lastFilm] = lastFilmRait;
+personalMovieDB.movies[lastFilm1] = lastFilmRait1;
+
+console.log(personalMovieDB.movies);
